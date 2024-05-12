cc8f870c16558cecc3ea960a80e6c0e9b6bba3ab
johnifemezuo <johnifemezuoanayo@gmail.com>
2024-05-12 23:11:38 +0100

    added mobile navigation

--- components/Global/Navigation/Navigator.tsx
+++ components/Global/Navigation/Navigator.tsx
@@ -1,76 +1,78 @@
-import Link from "next/link";
 import { useRouter } from "next/router";
 import { useState } from "react";
+import { Link as ScrollLink } from "react-scroll";
 import { TabLogo } from "../Icons/TabLogo";
 
 const NavLink = ({
   name,
   link,
-  active,
 }: {
   name: string;
   link: string;
-  active?: boolean;
 }) => {
   return (
-    <Link href={link}>
-      <button
-        className={`capitalize block rounded-3xl py-2 px-4  transition-all duration-300 ${
-          active
-            ? "bg-white text-black hover:bg-white"
-            : "bg-transparent hover:bg-white/10 text-white"
-        }`}
-      >
-        {name}
-      </button>
-    </Link>
+    <ScrollLink
+      to={link}
+      spy={true}
+      smooth={true}
+      duration={500}
+      className="capitalize w-[180px] md:w-auto block text-white cursor-pointer md:ml-0 rounded-3xl py-2.5 md:py-2 px-4  transition-all duration-300"
+      activeClass="bg-white/30"
+    >
+      {name}
+    </ScrollLink>
   );
 };
 
 export const Navigation = () => {
   const { pathname } = useRouter();
-  console.log(pathname);
 
   const router = useRouter();
   const { hash } = router.query;
 
   // Check if the hash exists in the current URL
-  const hashExists = hash && hash[0] === "packages";
   const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div
-      className={`z-50  md:mx-auto bg-black/80 md:rounded-full  py-3 md:max-w-[670px] px-3  fixed bottom-5 left-3 md:inset-x-0 ${
-        isOpen ? "rounded-lg " : "rounded-full"
+      className={`z-50  md:mx-auto bg-black/90 md:rounded-full  py-3 md:max-w-[670px] px-3  fixed bottom-5 left-3 md:inset-x-0 ${
+        isOpen ? "rounded-xl " : "rounded-full"
       }`}
     >
-      
-        <div className={`${isOpen ? "block" : "hidden"} items-center justify-between md:flex pt-2 md:pt-0`}>
-          <Link href="/#hero" className="-mb-1 ml-3 md:ml-0">
-            <button>
-              <TabLogo />
-            </button>
-          </Link>
+      <div
+        className={`${
+          isOpen ? "block" : "hidden"
+        } items-center justify-between block  md:flex md:space-y-0 space-y-1 pt-2 md:pt-0`}
+      >
+        <ScrollLink
+          to="hero"
+          spy={true}
+          smooth={true}
+          duration={500}
+          className="-mb-1 ml-3 md:ml-0"
+          activeClass="hero"
+        >
+          <button>
+            <TabLogo />
+          </button>
+        </ScrollLink>
 
-          <NavLink name="Intro" link="/#intro" active={pathname === "#intro"} />
-          <NavLink
-            name="services"
-            link="/#services"
-            active={pathname === "#services"}
-          />
-          <NavLink name="works" link="/#works" active={pathname === "#works"} />
-          <NavLink
-            name="packages"
-            link="/#packages"
-            active={pathname === "#packages"}
-          />
-          <NavLink name="about" link="#about" active={pathname === "#about"} />
-          <NavLink
-            name="contact us"
-            link="#contactus"
-            active={pathname === "#intro"}
-          />
-        </div>
+        <NavLink name="Intro" link="intro"  />
+        <NavLink
+          name="services"
+          link="services"
+        />
+        <NavLink name="works" link="works"  />
+        <NavLink
+          name="packages"
+          link="packages"
+        />
+        <NavLink name="about" link="about"  />
+        <NavLink
+          name="contact us"
+          link="contactus"
+        />
+      </div>
 
       <button
         onClick={() => setIsOpen(!isOpen)}
