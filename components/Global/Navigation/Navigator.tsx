a57b9878a7b4c9e549bbb06f3bc4644a68c51f7a
johnifemezuo <johnifemezuoanayo@gmail.com>
2024-05-12 21:52:15 +0100

    added mobile navigation

--- components/Global/Navigation/Navigator.tsx
+++ components/Global/Navigation/Navigator.tsx
@@ -77,13 +77,23 @@ export const Navigation = () => {
         onClick={() => setIsOpen(!isOpen)}
         className="text-white w-8 h-8 md:hidden px-1"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          fill="currentColor"
-        >
-          <path d="M3 4H21V6H3V4ZM9 11H21V13H9V11ZM3 18H21V20H3V18Z"></path>
-        </svg>
+        {isOpen ? (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+          >
+            <path d="M10.5859 12L2.79297 4.20706L4.20718 2.79285L12.0001 10.5857L19.793 2.79285L21.2072 4.20706L13.4143 12L21.2072 19.7928L19.793 21.2071L12.0001 13.4142L4.20718 21.2071L2.79297 19.7928L10.5859 12Z"></path>
+          </svg>
+        ) : (
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="currentColor"
+          >
+            <path d="M3 4H21V6H3V4ZM9 11H21V13H9V11ZM3 18H21V20H3V18Z"></path>
+          </svg>
+        )}
       </button>
     </div>
   );
