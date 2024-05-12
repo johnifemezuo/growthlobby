751f6e058788fee667f510cd17a93016d465665e
johnifemezuo <johnifemezuoanayo@gmail.com>
2024-05-12 17:17:34 +0100

    added mobile navigation

--- components/Global/Navigation/Header.tsx
+++ components/Global/Navigation/Header.tsx
@@ -1,18 +1,16 @@
 import Link from "next/link";
-import Button from "../Button/Button";
 import { Logo } from "../Icons/Logo";
 
 export const Header = () => {
   return (
-    <div id="hero"  className="flex items-center justify-between">
+    <div id="hero" className="flex items-center justify-between">
       <Logo className="w-[180px] md:w-[230px]" />
 
       <Link href="https://cal.com/john-ifemezuo/15min" target="_blank">
         <button className="bg-black border-2 hover:bg-white/5 hover:text-black transition-all duration-300 text-sm md:text-base text-white rounded-full font-medium border-black py-2 md:py-3 px-4  md:px-6 hover:shadow-xl">
-        Book a call
-  </button>
+          Book a call
+        </button>
       </Link>
-
     </div>
   );
 };
--- components/Global/Navigation/Navigator.tsx
+++ components/Global/Navigation/Navigator.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
-import Button from "../Button/Button";
-import { TabLogo } from "../Icons/TabLogo";
 import { useRouter } from "next/router";
+import { useState } from "react";
+import { TabLogo } from "../Icons/TabLogo";
 
 const NavLink = ({
   name,
@@ -15,7 +15,7 @@ const NavLink = ({
   return (
     <Link href={link}>
       <button
-        className={`capitalize rounded-3xl py-2 px-4  transition-all duration-300 ${
+        className={`capitalize block rounded-3xl py-2 px-4  transition-all duration-300 ${
           active
             ? "bg-white text-black hover:bg-white"
             : "bg-transparent hover:bg-white/10 text-white"
@@ -28,30 +28,63 @@ const NavLink = ({
 };
 
 export const Navigation = () => {
-  const {pathname} = useRouter();
+  const { pathname } = useRouter();
   console.log(pathname);
-  
+
   const router = useRouter();
   const { hash } = router.query;
 
   // Check if the hash exists in the current URL
-  const hashExists = hash && hash[0] === 'packages';
-  
-  return (
-    <div className="hidden z-50 md:flex mx-auto bg-black/80 rounded-full py-3 max-w-[670px] px-3 items-center justify-between fixed bottom-8 inset-x-0">
-      <Link href="/#hero" className="-mb-1">
-        <button
-        >
-          <TabLogo />
-        </button>
-      </Link>
+  const hashExists = hash && hash[0] === "packages";
+  const [isOpen, setIsOpen] = useState(false);
 
-      <NavLink name="Intro" link="/#intro" active={pathname === "#intro"} />
-      <NavLink name="services" link="/#services" active={pathname === "#services"}/>
-      <NavLink name="works" link="/#works" active={pathname === "#works"} />
-      <NavLink name="packages" link="/#packages" active={pathname === "#packages"}/>
-      <NavLink name="about" link="#about" active={pathname === "#about"}/>
-      <NavLink name="contact us" link="#contactus" active={pathname === "#intro"} />
+  return (
+    <div
+      className={`z-50  md:mx-auto bg-black/80 md:rounded-full  py-3 md:max-w-[670px] px-3  fixed bottom-8 left-3 md:inset-x-0 ${
+        isOpen ? "rounded-lg " : "rounded-full"
+      }`}
+    >
+      {isOpen && (
+        <div className="items-center justify-between md:flex">
+          <Link href="/#hero" className="-mb-1 hidden md:block">
+            <button>
+              <TabLogo />
+            </button>
+          </Link>
+
+          <NavLink name="Intro" link="/#intro" active={pathname === "#intro"} />
+          <NavLink
+            name="services"
+            link="/#services"
+            active={pathname === "#services"}
+          />
+          <NavLink name="works" link="/#works" active={pathname === "#works"} />
+          <NavLink
+            name="packages"
+            link="/#packages"
+            active={pathname === "#packages"}
+          />
+          <NavLink name="about" link="#about" active={pathname === "#about"} />
+          <NavLink
+            name="contact us"
+            link="#contactus"
+            active={pathname === "#intro"}
+          />
+        </div>
+      )}
+
+      <button
+        onClick={() => setIsOpen(!isOpen)}
+        className="text-white w-8 h-8 md:hidden px-1"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 24 24"
+          fill="currentColor"
+        >
+          <path d="M3 4H21V6H3V4ZM9 11H21V13H9V11ZM3 18H21V20H3V18Z"></path>
+        </svg>
+      </button>
     </div>
   );
 };
