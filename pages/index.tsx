b7f1139db1145faaeeb66de09c0bf12395871ee3
johnifemezuo <johnifemezuoanayo@gmail.com>
2024-05-30 12:11:55 +0100

    added tawk to

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import { Manrope, Farro } from "next/font/google";
 import { Layout } from "@/components/Global/Layout/Layout";
 import { HomePageIndex } from "@/components/Pages/HomePageIndex";
 import Head from "next/head";
+import { useEffect } from "react";
 
 const manrope = Manrope({
   weight: ["300", "400", "500", "700"],
@@ -10,6 +11,20 @@ const manrope = Manrope({
 });
 
 export default function Home() {
+  useEffect(() => {
+    const script = document.createElement('script');
+    script.async = true;
+    script.src = 'https://embed.tawk.to/66585c01981b6c5647766acd/1hv4ieqet';
+    script.charset = 'UTF-8';
+    script.setAttribute('crossorigin', '*');
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, []);
+
+  
   return (
     <>
       <Head>
